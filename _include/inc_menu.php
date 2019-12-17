267494544cd2026ea9e0bce43ab196f4926f9012
daba <dieng.daba@outlook.com>
2019-12-17 15:42:39 +0100

    gestion des droits d'utilisateurs

--- _include/inc_menu.php
+++ _include/inc_menu.php
@@ -1,22 +1,23 @@
 ﻿<?php
-if($_SESSION["status"] == 1){
-    $menu=array(
-        [hlien("tache","index"),"Tâche"], 
-        [hlien("categorie","index"),"Catégorie"],    
-        [hlien("utilisateur","index"),"Utilisateur"],  
-        [hlien("database","creer"),"RAZ BDD"],
-        [hlien("database","dataset"),"jeu de données"]
-    );    
-}else if ($_SESSION["status"] == 2) {
-    $menu=array(
-        [hlien("tache","index"),"Vos tâche"]
-    );
-
-}else{
-    $menu=""; 
-    $_SESSION["status"] = 0; 
+if (isset($_SESSION["status"])) {
+    if ($_SESSION["status"] == 1) {
+        $menu = array(
+            [hlien("tache", "index"), "Tâche"],
+            [hlien("categorie", "index"), "Catégorie"],
+            [hlien("utilisateur", "index"), "Utilisateur"],
+            [hlien("database", "creer"), "RAZ BDD"],
+            [hlien("database", "dataset"), "jeu de données"]
+        );
+    } else if ($_SESSION["status"] == 2) {
+        $menu = array(
+            [hlien("tache", "index"), "Vos tâches"]
+        );
+    }
+} else {
+    $menu = []; 
 }
+
 ?>
 <div class="myflexMenu">
-	<?php affiche_menu($menu); ?>		
+    <?php affiche_menu($menu); ?>
 </div>
